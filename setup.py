e96eabef91dfd05dada2ebc0ddfa6a15644f1d13
tizhproger <geralt194@gmail.com>
2025-04-04 01:43:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spamfilter",
+    version="0.1.0",
+    description="Spam classification library with TF-IDF and Transformer support",
+    author="Your Name",
+    author_email="your@email.com",
+    url="https://github.com/yourusername/spamfilter",
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn",
+        "transformers",
+        "datasets",
+        "torch",
+        "joblib",
+        "tqdm",
+        "nltk"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
