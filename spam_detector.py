e1f559735ce9f1c492457c880c1407d959d7bf01
tizhproger <geralt194@gmail.com>
2025-04-10 16:40:43 +0200

    Remastered BertLikeDetector + Detector classes for unification

--- spam_detector.py
+++ spam_detector.py
@@ -13,20 +13,24 @@ from utils import free_gpu, diagnostic_report
 torch.cuda.empty_cache()
 
 class Detector:
-    def __init__(self, model="tfidf", model_dir=None, load_if_exists=True):
+    def __init__(self, model="tfidf", models_dir=None, load_if_exists=True, num_labels=2) -> None:
         """
-        Initializes a Detector with the specified model.
+        Initializes a Detector instance with the specified model.
 
         Args:
-            model (str): The model to use. Defaults to "tfidf".
-            model_dir (str): The directory where the model is saved. Defaults to None.
-            load_if_exists (bool): If True and the model exists, loads the model. Defaults to True.
+            model (str): The name of the model to use. Defaults to "tfidf".
+            models_dir (Optional[str]): The directory where models are stored. If None, defaults to "models/{model_name}".
+            load_if_exists (bool): Whether to load an existing model if it exists. Defaults to True.
 
         Raises:
-            FileNotFoundError: If the model does not exist and load_if_exists is True.
+            FileNotFoundError: If the specified model is not found or is invalid and loading is required.
         """
         self.model_name = model.lower()
-        self.model_dir = model_dir or os.path.join("models", self.model_name)
+
+        if models_dir is None:
+            self.model_dir = os.path.join("models", self.model_name)
+        else:
+            self.model_dir = os.path.join(models_dir, self.model_name)
 
         model_exists = os.path.isdir(self.model_dir)
 
@@ -36,14 +40,15 @@ class Detector:
                 self.detector.load(self.model_dir)
 
         elif model_exists and Detector.is_valid_hf_model(self.model_dir) and load_if_exists:
-            self.detector = BertLikeDetector(model_name=model_dir)
+            self.detector = BertLikeDetector()
             self.detector.load(self.model_dir)
         
         elif not load_if_exists:
-            self.detector = BertLikeDetector(model_name=self.model_name)
+            self.detector = BertLikeDetector()
+            self.detector.initialize(self.model_name, num_labels=num_labels)
 
         else:
-            raise FileNotFoundError(f"Model directory '{self.model_dir}' not found or is invalid")
+            raise FileNotFoundError(f"Model '{self.model_name}' not found or is invalid")
     
     @staticmethod
     def is_valid_hf_model(path: str) -> bool:
@@ -58,21 +63,8 @@ class Detector:
         required_files = ["config.json", "pytorch_model.bin"]
         return all(os.path.isfile(os.path.join(path, f)) for f in required_files)
     
-    @classmethod
-    def custom(cls, model_name):
-        """Creates a Detector with a custom Hugging Face model.
-
-        Args:
-            model_name (str): The name of the Hugging Face model to use.
-            save_as (str, optional): The directory to save the model to. Defaults to None.
-
-        Returns:
-            Detector: A Detector instance with the specified model.
-        """
-        return cls(model=model_name, model_dir=None, load_if_exists=False)
-    
     @staticmethod
-    def list_models():
+    def list_models() -> list:
         """Lists all available models. Returns a list of strings, where each string is
         the name of a model. The list includes "tfidf" if the TF-IDF model is available.
         """
@@ -86,7 +78,7 @@ class Detector:
         ]
     
     @staticmethod
-    def is_model_available(name):
+    def is_model_available(name) -> bool:
         """
         Checks if a model with the given name is available.
 
@@ -99,7 +91,7 @@ class Detector:
 
         return name == "tfidf" or Detector.is_valid_hf_model(os.path.join("models", name))
 
-    def train(self, texts, labels, logging_dir=None, eval_texts=None, eval_labels=None, **kwargs):
+    def train(self, texts, labels, logging_dir=None, eval_texts=None, eval_labels=None, **kwargs) -> None:
         """
         Trains the model on given texts and labels.
 
@@ -115,7 +107,7 @@ class Detector:
         diagnostic_report(texts, labels)
         self.detector.train(texts, labels, logging_dir=logging_dir or "/logs", eval_texts=eval_texts, eval_labels=eval_labels, **kwargs)
 
-    def evaluate(self, texts, labels):
+    def evaluate(self, texts, labels) -> dict:
         """
         Evaluates the model on given texts and labels.
 
@@ -128,33 +120,22 @@ class Detector:
         """
         return self.detector.evaluate(texts, labels)
 
-    def predict(self, text_or_texts, return_proba=False):
+    def predict(self, text_or_texts, return_proba=False) -> list:
         """
-        Predicts the label for a single text or a list of texts.
+        Predicts labels for given texts.
 
         Args:
-            text_or_texts (str or list): A single text or a list of texts to predict the labels for.
+            text_or_texts (str or list): A text or a list of texts to predict.
+            return_proba (bool): If True, returns probabilities of labels instead of labels.
 
         Returns:
-            int: The predicted label for the given text(s).
+            list: Predicted labels or probabilities of labels.
         """
         if isinstance(text_or_texts, list):
             return self.detector.predict(text_or_texts, return_proba=return_proba)
         return self.detector.predict([text_or_texts], return_proba=return_proba)[0]
 
-    def predict_batch(self, texts):
-        """
-        Predicts the labels for a list of texts.
-
-        Args:
-            texts (list): A list of strings to predict the labels for.
-
-        Returns:
-            list: A list of predicted labels, one for each text in the input list.
-        """
-        return [self.predict(t) for t in texts]
-
-    def save(self, path):
+    def save(self, path) -> None:
         """
         Saves the current detector model to the specified directory.
 
@@ -163,7 +144,7 @@ class Detector:
         """
         self.detector.save(path)
 
-    def load(self, path):
+    def load(self, path) -> None:
         """
         Loads the model from the specified directory.
 
@@ -172,7 +153,7 @@ class Detector:
         """
         self.detector.load(path)
 
-    def get_name(self):
+    def get_name(self) -> str:
         """
         Returns the name of the model.
 
@@ -181,7 +162,7 @@ class Detector:
         """
         return self.model_name
     
-    def benchmark(self, texts):
+    def benchmark(self, texts) -> None:
         """
         Runs a benchmark of the model on the given list of texts.
 
@@ -195,7 +176,7 @@ class Detector:
         print(f"Time for {len(texts)} predictions: {time.time() - start:.2f} sec")
 
 class TFIDFDetector:
-    def __init__(self, max_features=5000, ngram_range=(1, 2)):
+    def __init__(self, max_features=5000, ngram_range=(1, 2)) -> None:
         """
         Initializes the TFIDFDetector with a TfidfVectorizer and LogisticRegression model.
 
@@ -220,7 +201,7 @@ class TFIDFDetector:
         self.model.fit(X, labels)
         self.is_fitted = True
 
-    def predict(self, texts: list, return_proba=False):
+    def predict(self, texts: list, return_proba=False) -> list:
         """
         Predicts labels for given texts.
 
@@ -259,7 +240,7 @@ class TFIDFDetector:
         X = self.vectorizer.transform(texts)
         return self.model.predict_proba(X)
 
-    def evaluate(self, texts: list, labels: list) -> None:
+    def evaluate(self, texts: list, labels: list) -> dict:
         """
         Evaluates the model on given texts and labels.
 
@@ -268,10 +249,20 @@ class TFIDFDetector:
         Args:
             texts (list): A list of texts to evaluate.
             labels (list): A list of labels for given texts.
+
+        Returns:
+            dict: A dictionary containing the accuracy score, confusion matrix, and classification report.
         """
-        y_pred = self.predict(texts)
-        print("Confusion matrix:\n", confusion_matrix(labels, y_pred))
-        print("\nClassification report:\n", classification_report(labels, y_pred))
+        preds = self.predict(texts)
+        matrix = confusion_matrix(labels, preds)
+        report = classification_report(labels, preds, output_dict=True)
+        acc = accuracy_score(labels, preds)
+        print("=== Confusion matrix ===")
+        print(matrix)
+        print("=== Report ===")
+        print(report)
+        print("Accuracy:", acc)
+        return {"accuracy": acc, "confusion_matrix": matrix, "classification_report": report}
 
     def save(self, path) -> None:
         """
@@ -298,24 +289,65 @@ class TFIDFDetector:
         self.is_fitted = True
 
 class BertLikeDetector:
-    def __init__(self, model_name="distilbert-base-uncased"):
+    def __init__(self) -> None:
         """
-        Initializes a BertLikeDetector with the specified model.
+        Initializes the BertLikeDetector instance.
+
+        Sets the model, tokenizer, pipeline, and model name to None. Sets the batch size
+        to 16 if a GPU is available, otherwise 4. Sets the maximum token length to 0 and
+        sets the is_ready flag to False.
+        """
+        self.model = None
+        self.tokenizer = None
+        self.pipeline = None
+        self.model_name = None
+        self.batch_size = 16 if torch.cuda.is_available() else 4
+        self.max_len = 0
+        self.is_ready = False
+    
+    def initialize(self, model_name, num_labels=2) -> None:
+        """
+        Initializes the BertLikeDetector with the specified model name and number of labels.
 
         Args:
-            model_name (str): The name of the model to use. Defaults to "distilbert-base-uncased".
+            model_name (str): The name of the pre-trained Hugging Face model to use.
+            num_labels (int): The number of labels for sequence classification. Defaults to 2.
+
+        Sets up the tokenizer and model using the specified model name, builds the
+        text classification pipeline, and marks the detector as ready.
         """
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
-        self.pipeline = None
-        self.is_trained = False
-        self.batch_size = 16 if torch.cuda.is_available() else 4
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
+        self._build_pipeline()
+        self.is_ready = True
+    
+    def _build_pipeline(self) -> None:
+        """
+        Builds a text classification pipeline for the model.
+
+        Sets the maximum token length based on the tokenizer's model_max_length,
+        adjusting to 512 if it exceeds 100,000. Initializes the pipeline with
+        truncation and padding enabled and sets the device to GPU if available.
+
+        Returns:
+            None
+        """
         self.max_len = self.tokenizer.model_max_length
         if self.max_len > 1e5:
             self.max_len = 512
 
-    def train(self, texts, labels, logging_dir="/logs", epochs=3, batch_size=8, eval_texts=None, eval_labels=None):
+        self.pipeline = TextClassificationPipeline(
+            model=self.model,
+            tokenizer=self.tokenizer,
+            truncation=True,
+            padding=True,
+            max_length=self.max_len,
+            device=0 if torch.cuda.is_available() else -1,
+            batch_size=self.batch_size
+        )
+
+    def train(self, texts, labels, logging_dir="/logs", epochs=3, batch_size=16 if torch.cuda.is_available() else 4, eval_texts=None, eval_labels=None) -> None:
         """
         Trains the model on given texts and labels.
 
@@ -323,14 +355,14 @@ class BertLikeDetector:
             texts (list): A list of texts to train.
             labels (list): A list of labels for given texts.
             logging_dir (str): The directory to save training logs to. Defaults to "/logs".
-            epochs (int): The number of epochs to train. Defaults to 3.
-            batch_size (int): The batch size to use for training. Defaults to 8 if GPU is available, otherwise 4.
+            epochs (int): The number of epochs to train for. Defaults to 3.
+            batch_size (int): The batch size for training. Defaults to 16 if GPU is available, otherwise 4.
             eval_texts (list): A list of texts to evaluate on.
             eval_labels (list): A list of labels for given texts to evaluate on.
-
-        Returns:
-            None
         """
+        if not self.is_ready:
+            raise ValueError("Model is not initialized or loaded.")
+
         dataset = Dataset.from_dict({"text": texts, "label": labels})
 
         def tokenize(example):
@@ -372,7 +404,7 @@ class BertLikeDetector:
 
         self.is_trained = True
 
-    def predict(self, texts, return_proba=False):
+    def predict(self, texts, return_proba=False) -> list:
         """
         Predicts labels for given texts.
 
@@ -383,18 +415,11 @@ class BertLikeDetector:
         Returns:
             list: Predicted labels or probabilities of labels.
         """
-        if not self.is_trained:
+        if not self.is_ready:
             raise ValueError("Model is not trained or loaded.")
 
         if self.pipeline is None:
-            self.pipeline = TextClassificationPipeline(
-                model=self.model,
-                tokenizer=self.tokenizer,
-                truncation=True,
-                padding=True,
-                max_length=self.max_len,
-                device=0 if torch.cuda.is_available() else -1,
-                batch_size=self.batch_size)
+            self._build_pipeline()
         
         outputs = self.pipeline(texts, batch_size=self.batch_size)
         if return_proba:
@@ -404,7 +429,7 @@ class BertLikeDetector:
             ]
         return [int(out["label"].split("_")[-1]) for out in outputs]
 
-    def predict_proba(self, texts):
+    def predict_proba(self, texts) -> list:
         """
         Predicts probabilities of all labels for given texts.
 
@@ -414,21 +439,16 @@ class BertLikeDetector:
         Returns:
             list: A list of lists, where each inner list contains probabilities of all labels for a text.
         """
+        if not self.is_ready:
+            raise ValueError("Model is not trained or loaded.")
+
         if self.pipeline is None:
-            self.pipeline = TextClassificationPipeline(
-                model=self.model,
-                tokenizer=self.tokenizer,
-                truncation=True,
-                padding=True,
-                max_length=self.max_len,
-                device=0 if torch.cuda.is_available() else -1,
-                batch_size=self.batch_size,
-                return_all_scores=True)
+            self._build_pipeline()
             
         results = self.pipeline(texts, batch_size=self.batch_size, return_all_scores=True)
         return [[score['score'] for score in output] for output in results]
 
-    def evaluate(self, texts, labels):
+    def evaluate(self, texts, labels) -> dict:
         """
         Evaluates the model on given texts and labels.
 
@@ -439,48 +459,45 @@ class BertLikeDetector:
             labels (list): A list of labels for given texts.
 
         Returns:
-            list: Predicted labels.
+            dict: A dictionary containing the accuracy score, confusion matrix, and classification report.
         """
-        preds = self.predict(texts)
-        print("=== Confusion matrix ===")
-        print(confusion_matrix(labels, preds))
-        print("=== Report ===")
-        print(classification_report(labels, preds))
-        print("Accuracy:", accuracy_score(labels, preds))
-        return preds
 
-    def save(self, path): 
+        if not self.is_ready:
+            raise ValueError("Model is not trained or loaded.")
+
+        preds = self.predict(texts)
+        matrix = confusion_matrix(labels, preds)
+        report = classification_report(labels, preds, output_dict=True)
+        acc = accuracy_score(labels, preds)
+        print("=== Confusion matrix ===")
+        print(matrix)
+        print("=== Report ===")
+        print(report)
+        print("Accuracy:", acc)
+        return {"accuracy": acc, "confusion_matrix": matrix, "classification_report": report}
+
+    def save(self, path) -> None: 
         """
         Saves the model and tokenizer to the specified path.
 
         Args:
             path (str): The directory to save the model and tokenizer to.
         """
-        if not os.path.exists(path):
-            os.makedirs(path)
+        if not self.is_ready:
+            raise ValueError("Model is not trained or loaded.")
 
+        os.makedirs(path, exist_ok=True)
         self.model.save_pretrained(path)
         self.tokenizer.save_pretrained(path)
 
-    def load(self, path):
+    def load(self, path) -> None:
         """
-        Loads the model and tokenizer from the specified path and initializes the pipeline.
+        Loads the model and tokenizer from the specified path and builds the pipeline.
 
         Args:
             path (str): The directory from which to load the model and tokenizer.
-
-        Raises:
-            ValueError: If the model or tokenizer cannot be loaded from the specified path.
         """
         self.model = AutoModelForSequenceClassification.from_pretrained(path)
         self.tokenizer = AutoTokenizer.from_pretrained(path)
-        self.pipeline = TextClassificationPipeline(
-            model=self.model,
-            tokenizer=self.tokenizer,
-            truncation=True,
-            padding=True,
-            max_length=self.max_len,
-            device=0 if torch.cuda.is_available() else -1,
-            batch_size=self.batch_size)
-        
-        self.is_trained = True
+        self._build_pipeline()
+        self.is_ready = True
