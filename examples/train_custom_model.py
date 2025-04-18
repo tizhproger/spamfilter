30a0b34c0e775475eb709a08faabbf8f9c9c5490
tizhproger <geralt194@gmail.com>
2025-04-18 14:09:11 +0200

    Fixed code due to updates (New way of trainig)

--- examples/train_custom_model.py
+++ examples/train_custom_model.py
@@ -5,9 +5,9 @@ df = pd.read_csv("your_dataset.csv")
 texts, labels = df["text"].tolist(), df["label"].tolist()
 X_train, X_test, y_train, y_test = train_test_split(texts, labels, test_size=0.2, random_state=42)
 
-model_name = "cointegrated/rubert-tiny2"
+model_name = "microsoft/deberta-v3-small"
 save_as = "models/rubert_custom"
 
-detector = Detector.custom(model_name)
+detector = Detector(model_name, load_if_exists=False)
 detector.train(X_train, y_train, eval_texts = X_test, eval_labels = y_test)
 detector.save(save_as)
